0b7ed26f40a21d66e050b3c615e5cf643741f266
LuannMateus <luanmateuslima34@gmail.com>
2021-09-13 09:30:18 -0300

    Add settings to deploy to the cloud

--- gdrive-webapi/src/index.js
+++ gdrive-webapi/src/index.js
@@ -1,3 +1,4 @@
+import http from 'http'
 import https from 'https';
 import fs from 'fs';
 import { logger } from './logger.js';
@@ -7,14 +8,20 @@ import Routes from './routes.js';
 const PORT = process.env.PORT || 3000;
 const HOSTNAME = 'localhost';
 
+const isProduction = process.env.NODE_ENV === 'production';
+process.env.USER = process.env.USER ?? 'system_user';
+
 const localHostSSL = {
   key: fs.readFileSync('./certificates/key.pem'),
   cert: fs.readFileSync('./certificates/cert.pem'),
 };
 
+const protocol = isProduction ? http : https;
+const ssConfig = isProduction ? {} : localHostSSL;
+
 const routes = new Routes();
 
-const server = https.createServer(localHostSSL, routes.handler.bind(routes));
+const server = protocol.createServer(ssConfig, routes.handler.bind(routes));
 
 const io = new Server(server, {
   cors: {
@@ -33,7 +40,9 @@ const startServer = () => {
   console.log(server.address());
   const { address, port } = server.address();
 
-  logger.info(`App is running at https://${address}:${port}`);
+  const protocol = isProduction ? 'http': 'https'
+
+  logger.info(`App is running at ${protocol}://${address}:${port}`);
 };
 
 server.listen(PORT, HOSTNAME, startServer);
